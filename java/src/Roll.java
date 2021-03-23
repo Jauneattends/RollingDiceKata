a45f235d8351333273efcc1e515d21ed107ce449
Jonathan PINA <jonathan.pina@outlook.fr>
2021-03-23 23:55:54 +0100

    Commit with approximative formula

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -16,9 +16,47 @@ public class Roll {
     private int nbRoll;
     private int modifier = 0;
     private int result = 0;
+    private String[] res;
+    private String[] res_2;
 
     //Construtor
     public Roll(String formula) {
+
+
+        if (formula.matches("^\\d?[d]\\d[+-]?\\d?$")) {
+
+            res = formula.split("d");
+
+            if (res.length == 1) {
+                if (res[0].matches("[+-]")) {
+                    res_2 = res[0].split("[+-]");
+                    this.diceValue = Integer.parseInt(res_2[0]);
+                    this.modifier = Integer.parseInt(res_2[1]);
+                } else {
+                    this.nbRoll = 1;
+                    this.diceValue = Integer.parseInt(res[0]);
+                    this.modifier = Integer.parseInt(res[1]);
+                }
+            }
+
+            if (res.length == 2) {
+                // Exemple 2d6+1
+                // res[0] = 2
+                // res[1] = 6+1 ou 6
+                // res_2 [0] = 6
+                // res_2 [1] = 1
+                if (res[1].matches("[+-]")) {
+                    res_2 = res[1].split("[+-]");
+                    this.nbRoll = Integer.parseInt(res[0]);
+                    this.diceValue = Integer.parseInt(res_2[0]);
+                    this.modifier = Integer.parseInt(res_2[1]);
+                } else {
+                    this.nbRoll = Integer.parseInt(res[0]);
+                    this.diceValue = Integer.parseInt(res[1]);
+                    this.modifier = 0;
+                }
+            }
+        } else this.statement = false;
 
     }
 
