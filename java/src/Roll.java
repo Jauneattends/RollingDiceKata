c2c34367d0866148ec3d3402af7a1c092f6992c5
Jonathan PINA <jonathan.pina@outlook.fr>
2021-03-23 22:09:22 +0100

    Commit without formula

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -1,5 +1,6 @@
 package src;
 
+import Random.RNG;
 
 public class Roll {
 
@@ -10,18 +11,53 @@ public class Roll {
     }
 
     // Attributes
+    private boolean statement = true;
+    private int diceValue;
+    private int nbRoll;
+    private int modifier = 0;
+    private int result = 0;
 
+    //Construtor
     public Roll(String formula) {
-        // TODO
+
     }
 
+    //Construtor
     public Roll(int diceValue, int nbRoll, int modifier) {
-        // TODO
+        if(diceValue <= 0) {
+            this.statement = false;
+        }
+        this.diceValue = diceValue;
+
+        if(nbRoll <= 0) {
+            this.statement = false;
+        }
+        this.nbRoll = nbRoll;
+
+        this.modifier = modifier;
+        this.result = result;
+
     }
 
     public int makeRoll(RollType rollType) {
-        // TODO
-        return 0;
-    }
+        Dice d = new Dice(this.diceValue);
+        if (!this.statement){
+            return -1;
+        }
 
+        int res = 0;
+        for (int i = 0; i < nbRoll ; i++) {
+            res += d.rollDice(); // res retourne le chiffre obtenu avec le lancer de dés.
+        }
+
+        res += this.modifier;
+
+        if (res < 0){
+            return 0;
+        }
+
+        System.out.println("Dice value : ");
+        System.out.println(res);
+        return res;
+    }
 }
